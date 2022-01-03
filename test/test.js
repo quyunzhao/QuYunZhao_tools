ef602798a75d69f3a2ec69af69c6f50414c32467
jingqu <1264352733@qq.com>
2022-01-03 20:25:51 +0800

    htmlUnEscape

--- test/test.js
+++ test/test.js
@@ -2,6 +2,10 @@ const m = require("../index");
 const date = m.dateFormate();
 console.log(date);
 
-const str = "<script>alert('ssss') &  </script>";
+const str = "<script type='text/javascript'>alert('123') &  </script>";
 
-console.log(m.htmlEscape(str));
+const strEscape = m.htmlEscape(str);
+
+const strUnEscape = m.htmlUnEscape(strEscape);
+console.log(strEscape);
+console.log(strUnEscape);
