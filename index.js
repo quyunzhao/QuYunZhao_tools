c50091d47320bb9a5636bdf9302d7887440b57fa
jingqu <1264352733@qq.com>
2022-01-03 19:51:50 +0800

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+// 包的入口文件
+
+// 格式化日期
+const dateFormate = (dataStr = new Date()) => {
+  const dt = new Date(dataStr);
+
+  const y = dt.getFullYear();
+
+  const m = addZero(dt.getMonth() + 1);
+
+  const d = addZero(dt.getDay());
+
+  const hh = addZero(dt.getHours());
+
+  const mm = addZero(dt.getMinutes());
+
+  const ss = addZero(dt.getSeconds());
+
+  return `${y}/${m}/${d} ${hh}:${mm}:${ss}`;
+};
+
+// 补零操作
+const addZero = (n) => {
+  return n < 9 ? "0" + n : n;
+};
